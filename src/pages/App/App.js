367bd837e278a3ecbabdb71cb0cad93384aae64a
Calvin Féau <calvin@Calvins-MacBook-Pro.local>
2019-07-16 19:01:26 -0700

    deploy

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -4,14 +4,14 @@ import userService from "../../utils/userService";
 // import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import RemainingItemsPage from '../../RemainingItemsPage/RemainingItemsPage'
+import RemainingItemsPage from '../../pages/RemainingItemsPage/RemainingItemsPage'
 import LogInPage from '../../pages/LogInPage/LogInPage';
-import MyFoodPage from '../../MyFoodPage/MyFoodPage';
-import MyListPage from '../../MyListPage/MyListPage';
-import NewItemPage from '../../NewItemPage/NewItemPage';
-import EditItemPage from '../../EditItemPage/EditItemPage';
-import ItemsAddedPage from '../../ItemsAddedPage/ItemsAddedPage'
-import SignUpPage from '../../SignUpPage/SignUpPage';
+import MyFoodPage from '../../pages/MyFoodPage/MyFoodPage';
+import MyListPage from '../../pages/MyListPage/MyListPage';
+import NewItemPage from '../../pages/NewItemPage/NewItemPage';
+import EditItemPage from '../../pages/EditItemPage/EditItemPage';
+import ItemsAddedPage from '../../pages/ItemsAddedPage/ItemsAddedPage'
+import SignUpPage from '../../pages/SignUpPage/SignUpPage';
 
 class App extends Component {
   constructor() {
